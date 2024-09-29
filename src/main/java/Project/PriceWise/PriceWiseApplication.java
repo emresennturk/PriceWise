eb2b3c43e1f8e03975c2d43dca2df2bbd56bb4de
emresennturk <combatia.fb.emre@hotmail.com>
2024-09-29 20:22:02 +0300

    first commit

---
+++ src/main/java/Project/PriceWise/PriceWiseApplication.java
@@ -0,0 +1,14 @@
+package Project.PriceWise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PriceWiseApplication {
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(PriceWiseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Project/PriceWise/PriceWiseApplicationTests.java
@@ -0,0 +1,13 @@
+package Project.PriceWise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PriceWiseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
